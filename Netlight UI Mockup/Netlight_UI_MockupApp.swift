8fd2928e04a200b16fa8cfd9a9c09995b8b03114
Jakob Körber <jakobkoerber@posteo.de>
2022-11-03 18:33:51 +0100

    Initial Commit

---
+++ Netlight UI Mockup/Netlight_UI_MockupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netlight_UI_MockupApp.swift
+//  Netlight UI Mockup
+//
+//  Created by Jakob Paul Körber on 03.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Netlight_UI_MockupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
